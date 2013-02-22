d2eb312d0527f256d9960725479f3f3ce77adc4b
Eduardo Turiño <eturino@gmail.com>
2013-02-22 03:30:32 +0100

    clean code

--- console_fallback.js
+++ console_fallback.js
@@ -1,5 +1,9 @@
 var console = window.console || {};
-console.log = console.log || function(){};
-console.warn = console.warn || function(){};
-console.error = console.error || function(){};
-console.info = console.info || function(){};
+console.log = console.log || function () {
+};
+console.warn = console.warn || function () {
+};
+console.error = console.error || function () {
+};
+console.info = console.info || function () {
+};
