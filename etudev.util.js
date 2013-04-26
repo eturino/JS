711878740aa69bcaaf0ee397f010da375659aa73
Eduardo Turiño <eturino@gmail.com>
2013-04-27 01:17:42 +0200

    format to PSR

--- etudev.util.js
+++ etudev.util.js
@@ -2,27 +2,27 @@
  * need jQuery
  */
 (function (window, $) {
-	'use strict';
+    'use strict';
 
-	var EtuDev = window.EtuDev || {};
+    var EtuDev = window.EtuDev || {};
 
-	/**
-	 * return all the classes in the DOM, with the number of elements that have them
-	 * @return {Object} class => #elements_with_class
-	 */
-	EtuDev.getAllClasses = function () {
-		var classes = {};
-		$('*').each(function () {
-			$($(this).attr('class').split(/\s+/)).each(function () {
-				if (this !== '') {
-					classes[this] = classes[this] || 0;
-					classes[this] += 1;
-				}
-			});
-		});
-		return classes;
-	};
+    /**
+     * return all the classes in the DOM, with the number of elements that have them
+     * @return {Object} class => #elements_with_class
+     */
+    EtuDev.getAllClasses = function () {
+        var classes = {};
+        $('*').each(function () {
+            $($(this).attr('class').split(/\s+/)).each(function () {
+                if (this !== '') {
+                    classes[this] = classes[this] || 0;
+                    classes[this] += 1;
+                }
+            });
+        });
+        return classes;
+    };
 
-	window.EtuDev = EtuDev;
+    window.EtuDev = EtuDev;
 
 })(window, jQuery);
